06c7d544afa2c85e35738ae1ec85a2384adf8ce5
wongvian <aor_user@hotmail.com>
2021-11-08 20:02:27 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+import { defineNuxtConfig } from 'nuxt3'
+import { presetAttributify, presetUno, presetIcons } from 'unocss'
+
+export default defineNuxtConfig({
+  meta: {
+    title: 'Vitesse Nuxt 3',
+  },
+  buildModules: [
+    '@vueuse/core/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+  ],
+  unocss: {
+    shortcuts: [
+      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ],
+    presets: [
+      presetUno(),
+      presetAttributify(),
+      presetIcons({
+        scale: 1.2,
+      }),
+    ],
+  },
+})
